c902b215e90b80be15db74f5e3c94055ecd74d43
Thuong Nguyen Van <nvthuong1996@gmail.com>
2020-11-20 15:06:20 +0700

    update

---
+++ pages/course.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="course-detail-container">abcd</div>
+</template>
+
+<script>
+export default {
+  auth: false,
+  layout: 'landing-layout',
+}
+</script>
+
+<style lang="scss">
+.test123 {
+  color: red;
+}
+</style>
